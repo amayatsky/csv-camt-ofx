95195ddd0934d04ba3adebf2defe2cbd0eb6e73c
Alexander Mayatsky <atrinax@gmail.com>
2023-02-23 16:22:16 +0100

    Rework to support German CSV CAMT format

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
-# Ofxer
+# CAMT Ofxer
 
 [![MIT License](http://img.shields.io/badge/license-MIT-blue.svg?style=flat)](LICENSE)
 
 <img src="./ofxer.png" width="400">
 
-CSV to OFX converter.
-A script of converting CSV files exported from your credit and bank account
+CSV CAMT to OFX converter tuned to German banking format.
+A script for converting CSV CAMT files exported from your bank account.
 
 > mostly using [csv2ofx](https://github.com/reubano/csv2ofx)
 
@@ -17,26 +17,23 @@ $ pip install csv2ofx pandas
 ## Usage
 ### terminal
 ```sh
-$ python ofxer.py -s 1 -c 0 1 2 your_credit.csv -o credit.ofx
-$ python ofxer.py -s 1 -c 0 1 2 4 your_bank.csv -o bank.ofx
+$ python ofxer.py -c 1 4 11 14 your_bank.csv -o bank.ofx
 ```
 
 ```
 $ python ofxer.py --help
-usage: ofxer.py [-h] [-p PARSER] [-o OUTPUT] -s SKIPROWS [-e ENCODING] -c
-                USECOLS [USECOLS ...]
-                csvfile
+usage: ofxer.py [-h] [-p PARSER] [-o OUTPUT] [-s SKIPROWS] [-e ENCODING] -c USECOLS [USECOLS ...] csvfile
 
-CSV to OFX converter.
-A script of converting CSV files exporting from your credit and bank account
+CSV CAMT to OFX converter tuned to German banking format.
+A script for converting CSV CAMT files exported from your bank account.
 
 positional arguments:
   csvfile               csv file exported from your credit or bank acount
 
-optional arguments:
+options:
   -h, --help            show this help message and exit
   -p PARSER, --parser PARSER
-                        specify the date format if special e.g. '%Y年%m月%d日'
+                        specify the date format if special e.g. 'd/m/Y'
   -o OUTPUT, --output OUTPUT
                         path to write ofx file (default: output.ofx)
   -s SKIPROWS, --skiprows SKIPROWS
@@ -45,20 +42,17 @@ optional arguments:
                         file encoding
   -c USECOLS [USECOLS ...], --usecols USECOLS [USECOLS ...]
                         column index number of
-                          date title amount
-                          (e.g. --usecols 0 4 5)
-                          or
-                          date title withdraw deposit
-                          (e.g. --usecols 0 3 10 5)
+                          date memo title amount
+                          (e.g. --usecols 1 4 11 14)
                           Note: counting from ZERO
 ```
 
-### your python script
+### Python
 ```python
 from ofxer import Ofxer
 options = {
-    'credit1': {'skiprows': 10, 'usecols': [0, 1, 8]   },
-    'credit2': {'skiprows':  7, 'usecols': [0, 1, 2]   },
+    'credit1': {'skiprows': 10, 'usecols': [0, 1, 8, 13]   },
+    'credit2': {'skiprows':  7, 'usecols': [0, 1, 2, 16]   },
     'bank':    {'skiprows':  1, 'usecols': [0, 1, 4, 3]},
     }
 credit1 = Ofxer('data/credit1.csv', options['credit1'])
@@ -66,46 +60,5 @@ print(credit1._df)
 credit1.write_ofx('credit1.ofx')
 ```
 
-### License
+## License
 This code is released under the MIT License, see [LICENSE](LICENSE)
-
-
-# Ofxer (In Japanese)
-
-クレジットカードや銀行のcsvをOFXフォーマットに変換するスクリプト
-
-## 使い方
-### ターミナル
-```sh
-$ python ofxer.py -s 1 -c 0 1 2 -e cp932 your_credit.csv -o credit.ofx
-$ python ofxer.py -s 1 -c 0 1 2 4 -e cp932 your_bank.csv -o bank.ofx
-```
-
-```
--s csvファイルの先頭を飛ばす行数(ヘッダーを含む)
--c 日付 タイトル 支払額
-　　または
-   日付 タイトル 出金額 入金額
-   の列番号　※カウントは0から
--e 文字コードの指定
--o ofxファイルの出力先
-```
-
-### Python
-```python
-from ofxer import Ofxer
-
-options = {
-    'orico':   {'skiprows': 10, 'usecols': [0, 1, 8],    'encoding': 'cp932'},
-    'view':    {'skiprows':  7, 'usecols': [0, 1, 2],    'encoding': 'cp932'},
-    'vpass':   {'skiprows':  1, 'usecols': [0, 1, 2],    'encoding': 'cp932'},
-    'sony':    {'skiprows':  1, 'usecols': [0, 1, 4, 3], 'encoding': 'cp932'},
-    'shinsei': {'skiprows':  1, 'usecols': [0, 1, 2, 3], 'encoding': 'cp932'},
-    }
-sony = Ofxer('data/YenFutsuRireki.csv', options['sony'])
-sony.write_ofx('sony.ofx')
-shinsei = Ofxer('data/JPY_CH_.csv', options['shinsei'])
-print(shinsei._df)
-shinsei.write_ofx('shinsei.ofx')
-```
-
--- ofxer.py
+++ ofxer.py
@@ -1,13 +1,13 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# Copyright (c) 2018 MXXIV.net
+# Copyright (c) 2023 MXXIV.net, Alexander Mayatsky
 #
 # This software is released under the MIT License.
 # http://opensource.org/licenses/mit-license.php
 
 """
-CSV to OFX converter.
-A script of converting CSV files exporting from your credit and bank account
+CSV CAMT to OFX converter tuned to German banking format.
+A script for converting CSV CAMT files exported from your bank account.
 """
 
 import csv
@@ -45,33 +45,21 @@ class Ofxer:
             if k not in option:
                 option[k] = None
 
-        # switch loading mode
         col_num = len(option['usecols'])
-        if col_num == 3:
-            self.is_bank = False
-        elif col_num == 4:
-            self.is_bank = True
-        else:
-            raise AttributeError("length of option['usecols'] must be 3 or 4")
+        if col_num != 4:
+            raise AttributeError("length of option['usecols'] must be 4")
 
         self._df = self.__load_csv(csvfile, option)
 
     def __load_csv(self, csvfile, option):
 
-        if self.is_bank:
-            colnames = ('date', 'title', 'withdraw', 'deposit')
-        else:
-            colnames = ('date', 'title', 'amount')
-
-        df = pd.read_csv(csvfile, index_col=0,
+        colnames = ['date', 'memo', 'title', 'amount']
+        
+        df = pd.read_csv(csvfile, index_col=0, sep=';',
                          skiprows=option['skiprows'],
                          usecols=option['usecols'],
                          encoding=option['encoding'],
                          names=colnames)
-
-        # courtesy for Japanese
-        if option['parser'] is None:
-            df.index = df.index.str.replace(r'[年月日]', '/')
 
         # parse datetime and remove invalid rows
         def __try_todate(text):
@@ -83,45 +71,42 @@ class Ofxer:
         df = df[df.index.map(lambda x: __try_todate(x))]
         df.index = pd.to_datetime(df.index, format=option['parser'])
 
-        # make missing value to zero
-        if self.is_bank:
-            df['withdraw'].fillna(0, inplace=True)
-            df['deposit'].fillna(0, inplace=True)
+        # make missing value to empty string
+        df['title'].fillna('', inplace=True)
+        df['memo'].fillna('', inplace=True)
+        df['amount'].fillna(0, inplace=True)
         df = df[~df.isnull().any(axis=1)]
 
         # trim special characters
         def __to_num(df):
             if df.dtype == 'object':
-                not_num_or_dot = r'([^\d\.])'
-                df = df.str.replace(not_num_or_dot, '').fillna(0)
+                df = df.str.replace('.', '', regex=False) # 1.890,41 -> 1890,41
+                df = df.str.replace(',', '.', regex=False) # 1890,41 -> 1890.41
                 df = df.astype(float)
             return df
 
-        if self.is_bank:
-            df['deposit'] = __to_num(df['deposit'])
-            df['withdraw'] = __to_num(df['withdraw'])
-            df['amount'] = df['deposit'].add(-df['withdraw'], fill_value=0)
-        else:
-            df['amount'] = -(__to_num(df['amount']))
-
+        df['amount'] = __to_num(df['amount'])
+        
         return df
 
-    def write_ofx(self, ofxfile):
+    def write_ofx(self, ofxfile, debug=False):
         """ write out ofxfile from DataFrame """
         mapping = {
             'account': 'account',
             'date': itemgetter('date'),
             'payee': itemgetter('title'),
             'amount': itemgetter('amount'),
+            'desc': itemgetter('memo')
             }
 
         ofx = OFX(mapping)
         data = self._df.to_csv(quoting=csv.QUOTE_ALL)
+        if debug:
+            print(self._df)
         records = read_csv(StringIO(data))
         groups = ofx.gen_groups(records)
         cleaned_trxns = ofx.clean_trxns(groups)
         data = utils.gen_data(cleaned_trxns)
-
         content = it.chain([ofx.header(), ofx.gen_body(data), ofx.footer()])
 
         with open(ofxfile, 'wb') as f:
@@ -133,15 +118,15 @@ def col_act():
     # Note: https://stackoverflow.com/questions/4194948/python-argparse-is-there-a-way-to-specify-a-range-in-nargs
     class ColumnAction(argparse.Action):
         def __call__(self, parser, args, values, option_string=None):
-            nmin = 3
+            nmin = 4
             nmax = 4
             if not nmin <= len(values) <= nmax:
                 msg = 'argument "{f}" requires between {nmin} and {nmax} arguments'.format(
                     f=self.dest, nmin=nmin, nmax=nmax)
                 raise argparse.ArgumentTypeError(msg)
-            # check dupulication
+            # check duplication
             if len(values) != len(set(values)):
-                msg = 'argument "{f} {v}" dupulicated'.format(
+                msg = 'argument "{f} {v}" duplicated'.format(
                     f=self.dest, v=values)
                 raise argparse.ArgumentTypeError(msg)
 
@@ -152,24 +137,21 @@ def col_act():
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
                 prog='ofxer.py',
-                description='CSV to OFX converter.\nA script of converting CSV files exporting from your credit and bank account',
+                description='CSV CAMT to OFX converter tuned to German banking format.\nA script for converting CSV CAMT files exported from your bank account.',
                 formatter_class=argparse.RawTextHelpFormatter,
                 epilog='-------------------------------------------------------------------------------',
                 add_help=True,
                 )
 
     parser.add_argument('csvfile',          action='store',   nargs=None,                          help='csv file exported from your credit or bank acount')
-    parser.add_argument('-p', '--parser',   action='store',   nargs=None, default=None,            help="specify the date format if special e.g. '%%Y年%%m月%%d日'")
+    parser.add_argument('-p', '--parser',   action='store',   nargs=None, default='%d.%m.%y',      help="specify the date format if special e.g. 'y/m/d")
     parser.add_argument('-o', '--output',   action='store',   nargs=None, default='output.ofx',    help='path to write ofx file (default: output.ofx)')
-    parser.add_argument('-s', '--skiprows', action='store',   nargs=None, required=True, type=int, help='skipping number of csv file headers (incl. column name)')
-    parser.add_argument('-e', '--encoding', action='store',   nargs=None, default=None,            help='file encoding')
+    parser.add_argument('-s', '--skiprows', action='store',   nargs=None, default=1, type=int,     help='skipping number of csv file headers (incl. column name)')
+    parser.add_argument('-e', '--encoding', action='store',   nargs=None, default='unicode_escape',help='file encoding')
     parser.add_argument('-c', '--usecols',  action=col_act(), nargs='+',  required=True, type=int, help=textwrap.dedent('''\
                                                                                                         column index number of
-                                                                                                          date title amount
-                                                                                                          (e.g. --usecols 0 4 5)
-                                                                                                          or
-                                                                                                          date title withdraw deposit
-                                                                                                          (e.g. --usecols 0 3 10 5)
+                                                                                                          date memo title amount
+                                                                                                          (e.g. --usecols 1 4 11 14)
                                                                                                           Note: counting from ZERO
                                                                                                         '''))
 
@@ -179,9 +161,8 @@ if __name__ == '__main__':
                'skiprows': args.skiprows,
                'usecols': args.usecols,
                'encoding': args.encoding}
-    print(options)
 
     ofxer = Ofxer(args.csvfile, options)
-    ofxer.write_ofx(args.output)
+    ofxer.write_ofx(args.output, True)
 
     print('Successfully converted to {}'.format(args.output))
